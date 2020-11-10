8f3cc89d41d9e3439665f583b19f91fa0d6cdb32
Mik <Berezhanov.M@gmail.com>
2020-11-10 13:59:42 +0300

    chapter 8, 9 added

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ void chapter8 (void);
 void chapter9 (void);
 void chapter10 (void);
 void chapter11 (void);
+void chapter12 (void);
+void chapter13 (void);
 
 int a;	// в глобальной области видимости объект встроенного типа инициализируется 0, но не в локальной
 //const int c ;	// ОШИБКА: Необходима инициализация константы
@@ -132,6 +134,10 @@ int main(int argc, char* argv[])
 	//chapter6();
 
 	chapter7();
+
+	//chapter8();
+
+	chapter9();
 
 	return 0;
 }
@@ -509,3 +515,201 @@ void chapter7 (void)
 	s.combine(static_cast<Sales_data>(str));	// ПРИМ: создается временный объект Sales_data из строки
 }
 
+
+#include <fstream>
+#include <sstream>
+
+std::istream& my_print (std::istream& is)
+{
+	while(!is.eof())
+	{
+		std::string buf;
+		is >> buf;
+		std::cout << buf << std::endl;
+	}
+	 
+	std::cout << "end of file" << std::endl;
+	is.clear();
+
+	return is;
+}
+
+int my_fread(const std::string& fname)
+{
+	std::string str;
+	std::vector<std::string> vec;
+
+	std::ifstream file(fname);	// Открыть файл в режиме in
+
+	if(!file.is_open()) std::cerr << "file '" << fname << "' open failed" << std::endl;
+
+	while(getline(file, str)) vec.push_back(str);	//Помещает каждую строку в вектор
+	//while(file >> str) vec.push_back(str);	//Помещает каждое слово в вектор
+
+	for(auto &it : vec) std::cout << it << std::endl;
+
+	file.close(); // Закрывается неявно когда удаляется объект fstream
+}
+
+void chapter8 (void)
+{
+	int ival;
+	using std::cin;
+	using std::cout;
+	using std::endl;
+
+	//my_print(cin);
+
+	//while(cin >> ival);
+
+	cout << "fail: " << cin.fail() << " badbit: " << cin.bad() <<
+	" eof:" << cin.eof() << endl;
+
+	//my_fread("makefile");
+
+	// ПРИМ, по умолчанию файл открывается в режиме out | trunc (усекается)
+	// Открыть файл в режиме дозаписи (не стирая содержимое).
+	std::ofstream out("file", std::ofstream::app); // out установлен неявно
+
+	std::string my_str("first_word 2nd_word 3d_word 4th_word");
+	std::string word;
+	std::istringstream istr(my_str);// консруктор - поток содержит копию строки
+	istr.str(my_str);				// копировать строку в поток 
+	istr.str();						// вернуть копию строки, которую хранит поток
+	// ПРИМ. можно читать из потоко строк istr по одному слову за раз
+	istr >> word;;
+	cout << word << endl;
+	//my_print(istr);
+
+	std::string my_str2;
+	std::istringstream istr2(my_str2);
+	my_str2 = my_str;
+	//istr2.str(my_str2);
+	cout << "istr2 contains: " << istr2.str() << endl;
+}
+
+
+
+
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <array>
+
+static void read_str_to_deque()
+{
+	using std::string; using std::deque; using std::cin; using std::cout; using std::endl; using std::list;
+
+	string word;
+#ifdef DEQUE
+	cout << "[ " << __func__ << "() ] --- Using deque as container" << endl;
+	deque<string> c;
+#else
+	cout << "[ " << __func__ << "() ] --- Using list as container" << endl;
+	list<string> c;
+#endif
+
+	auto iter = c.begin();
+	// ПРИМ insert, emplace выполняют операции с эт-ом ПЕРЕД, обозначенным итератором
+	// ПРИМ при изменении контейнера (добавление \ удаление эл-тов) его итераторы становятся недействительными
+	// (инвалидация итераторов) поэтому в теле цикла необходимо обновлять интератор, а не кэшировать их заранее
+	// данный цикл аналогичен вызовам push_front()
+	while(cin >> word) iter = c.insert(iter, word);	// insert возвращает итератор на добавленный эл-т
+
+	cin.clear();
+	cout << "-------------------------------------------" << endl;
+	cout << "container size: " << c.size() << endl;
+
+
+	auto tmp = c.end();
+	auto t = c.erase(tmp, tmp);	// удалить диапазон между двумя итераторами (в данном случае ничего не удаляется)
+						
+	for(auto &it : c) cout << it << endl;
+}
+
+// параметр - ссылка на массив из 12 эл-тов ттипа int
+static void arr_remove_elems(int(&arr)[12])
+{
+	std::vector<int> vi(std::begin(arr), std::end(arr));
+	std::list<int> li(std::begin(arr), std::end(arr));
+
+	auto v_it = vi.begin();
+	auto l_it = li.begin();
+
+	// удалить из вектора четные эл-ты
+	while(v_it != vi.end()){
+		if(*v_it % 2) ++v_it;		// переход на следующий эл-т
+		else v_it = vi.erase(v_it);	// возвращает итератор на эл-т сразу после последнего удаленного
+	}
+	std::cout << "Vector without even elements: " << std::endl;
+	for(auto &it : vi) std::cout << it << std::endl;
+
+	// удалить из списка нечетные эл-ты
+	while(l_it != li.end()){
+		if(*l_it % 2) l_it = li.erase(l_it);
+		else ++l_it;
+	}
+	std::cout << "List without odd elements: " << std::endl;
+	for(auto &it : li) std::cout << it << std::endl;
+}
+
+void chapter9 (void)
+{
+	std::list<std::list<int>> example;
+
+	std::list<int> l;
+	l.emplace(l.begin(), 12);
+	std::cout << "list size: " << l.size() << std::endl;
+
+	std::array<char, 10> carr = {'a', 0x31, 'b', 0x43};
+	std::array<char, 10>::size_type idx = 0;
+	carr[idx] = 'v';
+
+	std::vector<int> vec1;			// Инициализация стандартным конструкторм (пустой вектор)
+	std::vector<int> vec2(10);		// Создание 10 элементов, инициализация по умолчанию (0)
+	std::vector<int> vec3(10, 43);	// Создание 10 эл-в и их инициализация значением 43
+	std::vector<int> vec4(vec3);	// Конструктор копирования (типы должны совпадать)
+	std::vector<int> vec5 = {1, 2, 3, 4, 5};	// Инициализация списком инициализации
+	std::vector<int> vec6(vec5.begin(), vec5.end());	// Конструктор получающий два интератора для копирования (диапазон)
+
+
+	
+	std::list<int> la{1, 2, 3, 4, 5};
+	std::vector<double> va(la.begin(), la.end());
+
+	// ОШИБКА сравнения котнейнеров разных типов
+	//if(la == va) std::cout << "Container copy succeed" << std::endl;
+	auto it_vec = va.begin();
+	for(auto it_list = la.begin(); it_list != la.end() && it_vec != va.end(); ++it_list, ++it_vec)
+	{
+		if(*it_list != *it_vec) std::cout << "Container copy failed" << std::endl;
+	}
+
+	std::vector<char> vt;
+	//vt[0] = 'a';		ОШИБКА. Вектор пуст, нет элементов
+	try{
+		vt.at(0) = 'a';	// возбудит исключение, но не крах программы
+	}
+	catch(std::out_of_range err){
+		std::cout << "[ TEST ] Exception occured: " << err.what() << std::endl;
+	}
+
+	if(!vt.empty()){
+		vt.front() = 'a';	// требуют проверки наличия эл-тов в контейнере
+		*vt.begin() = 'a';
+	}
+	
+
+	vt.insert(vt.begin(), 10, 'a');	// вставить 10 элементов 'a' перед итератором begin()
+	std::vector<Sales_data> vsales;
+	vsales.emplace(vsales.begin(), "666");	// вызывает конструктор класса объекта, создает объект в контейнере
+	vsales.emplace_back();	// используется конструктор Sales_data по умолчанию
+	vsales.emplace(vsales.end(), "999", 25, 15.99);	// аргументы должны соответствовать конструктору типа эл-та
+
+
+	//read_str_to_deque();
+
+
+	int ai[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+	arr_remove_elems(ai);
+}
