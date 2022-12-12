65f24d6b73130797815c918a2b495e45b05fa235
Mikhail Berezhanov <mpb@martistel.com>
2022-12-12 10:44:05 +0300

    shared_ptr usage added

--- smart_pointers/usage.cpp
+++ smart_pointers/usage.cpp
@@ -14,30 +14,30 @@ struct A
 	void msg() { cout << "A::msg" << endl; }
 };
 	
-void pass_by_ref(const unique_ptr<A>& up)
+void up_pass_by_ref(const unique_ptr<A>& up)
 {
 	up->msg();
 
-	cout << "passed_by_ref exits" << endl;
+	cout << "up_pass_by_ref exits" << endl;
 }
 
-void pass_by_value(unique_ptr<A> up)
+void up_pass_by_value(unique_ptr<A> up)
 {
 	up->msg();
 
-	cout << "pass_by_value exits" << endl;
+	cout << "up_pass_by_value exits" << endl;
 }
 
 void unique_ptr_passing_test()
 {
-	cout << "----------------------------" << endl;
+	cout << "---unique_ptr_passing_test---" << endl;
 
 	{
 		auto aptr = make_unique<A>();	// A::Ctr
 		
-		pass_by_ref(std::move(aptr));	// no movement take place
+		up_pass_by_ref(std::move(aptr));	// no movement take place
 
-		cout << "test 1 exiting" << endl;
+		cout << "unique_ptr test 1 exiting" << endl;
 	} // A::Dtr
 	
 	cout << "----------------------------" << endl;
@@ -45,27 +45,110 @@ void unique_ptr_passing_test()
 	{
 		auto aptr = make_unique<A>();	// A::Ctr
 		
-		pass_by_value(std::move(aptr)); // ownership now inside function
+		up_pass_by_value(std::move(aptr)); // ownership now inside function
 		// A::Dtr
 
-		cout << "test 2 exiting" << endl;
+		cout << "unique_ptr test 2 exiting" << endl;
 	}
 
 	cout << "----------------------------" << endl;
 
 	{		
-		pass_by_ref(make_unique<A>()); // A::Ctr (temp object created)
+		up_pass_by_ref(make_unique<A>()); // A::Ctr (temp object created)
 		// A::Dtr
 
-		cout << "test 3 exiting" << endl;
+		cout << "unique_ptr test 3 exiting" << endl;
 	}
 
 	cout << "----------------------------" << endl;
 }
 
 
+void sp_pass_by_ref(const std::shared_ptr<A>& sp)
+{
+	cout << "sp_pass_by_ref entered" << endl;
+	cout << "use count: " << sp.use_count() << endl; 
+	std::shared_ptr<A> sp2 = sp;
+	sp2->msg();
+	cout << "use count: " << sp2.use_count() << endl; 
+	cout << "sp_pass_by_ref exits" << endl;
+}  // sp2 out of scope - (use_count - 1)
+
+// sp is a copy , so (use_count + 1)
+void sp_pass_by_value(std::shared_ptr<A> sp)
+{
+	cout << "sp_pass_by_value entered" << endl;
+	cout << "use count: " << sp.use_count() << endl; 
+
+	std::shared_ptr<A> sp2 = sp;
+	sp2->msg();
+	cout << "use count: " << sp2.use_count() << endl;
+
+	cout << "sp_pass_by_value exits" << endl;
+} // sp, sp2 out of scope - (use_count - 2)
+
+void shared_ptr_passing_test()
+{
+	cout << "---shared_ptr_passing_test---" << endl;
+
+	{
+		auto aptr = make_shared<A>();	// A::Ctr
+		cout << "use count: " << aptr.use_count() << endl;	// 1 
+		sp_pass_by_ref(aptr);								// 1 -> 2 -> 1
+		cout << "use count: " << aptr.use_count() << endl;	// 1
+		cout << "shared_ptr test 1 exiting" << endl;
+	} // A::Dtr
+	
+	cout << "----------------------------" << endl;
+
+	{
+		auto aptr = make_shared<A>();	// A::Ctr
+		cout << "use count: " << aptr.use_count() << endl;	// 1 
+		// ownership now shared with function scope
+		sp_pass_by_value(aptr); // 2 (copied to func arg) -> 3 -> (exiting scope) 2 -> 1
+		cout << "use count: " << aptr.use_count() << endl;	// 1
+		cout << "shared_ptr test 2 exiting" << endl;
+	} // A::Dtr
+
+	cout << "----------------------------" << endl;
+
+	{		
+		auto aptr = make_shared<A>();
+		cout << "use count: " << aptr.use_count() << endl;	// 1 
+
+		// ownership now inside thefunction scope
+		sp_pass_by_value(std::move(aptr));	// 1 -> 2 -> 1 -> 0 (A::Dtr)	
+
+		cout << "use count: " << aptr.use_count() << endl;	// 0
+		cout << "shared_ptr test 3 exiting" << endl;
+	}
+
+	cout << "----------------------------" << endl;
+
+	{		
+		auto aptr = make_shared<A>();
+		cout << "use count: " << aptr.use_count() << endl;	// 1 
+
+		// no movement take place - works as just passing by ref
+		sp_pass_by_ref(std::move(aptr));					// 1 -> 2 -> 1
+
+		cout << "use count: " << aptr.use_count() << endl;	// 1
+		cout << "shared_ptr test 4 exiting" << endl;
+	} // A::Dtr
+
+	cout << "----------------------------" << endl;
+}
+
+
+
+
+
 int main()
 {
+	unique_ptr_passing_test();
+
+	shared_ptr_passing_test();
+
 	// Unique ptr features:
 	// - doesn't have Control block.
 	// - if no custom deleter provided has size == raw ptr (sizeof(void*))
@@ -80,7 +163,11 @@ int main()
 
 	// NOTE: decltype(function) deduce function signature ( i.e. int(FILE*) ). 
 	// To make deduction as function pointer - use '*' or '&function'. ( int(*)(FILE*) ) 
-	unique_ptr< FILE, decltype(fclose)* > uni_file(fopen("uni_file.txt", "w+"), fclose);
+	{
+		unique_ptr< FILE, decltype(fclose)* > uni_file(fopen("uni_file.txt", "w+"), fclose);
+	}
+	
+	remove("uni_file.txt");
 
 	// Shared ptr custom deleter is not type but constructor parameter.
 	//
@@ -113,8 +200,6 @@ int main()
 	std::shared_ptr<double> sptr2(pi); 
 	// Control block for resource pi is created again. 
 	// This will cause double delete of pi !!
-
-	unique_ptr_passing_test();
 
 	return 0;
 }
